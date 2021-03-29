7472680a0d7afa878fca40b98e9b865976c5c41b
joeljdv <joeldiazjdv@gmail.com>
2021-03-29 19:17:35 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name,email){
+    return fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        "name": `${name}`,
+        "email": `${email}`
+    })
+} )
+    .then(res => res.json())
+    .then((data) => {
+        const newDiv = document.createElement('div')
+        newDiv.innerHTML = data.id;
+        document.body.appendChild(newDiv)
+    } )
+    .catch((error)=> {
+        alert("ERROR!!")
+       const newDiv = document.createElement("div")
+       newDiv.innerHTML=error
+       document.body.appendChild(newDiv)
+    })
+}
+
+ 
